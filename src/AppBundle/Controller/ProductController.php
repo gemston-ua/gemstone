6db7f6c41c7c8002497d733201eb717c58497c60
Alex Moshta <ahonymous@gmail.com>
2016-01-19 23:22:05 +0200

    added form to product and new action

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -3,10 +3,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
+use AppBundle\Form\ProductType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/products")
@@ -21,7 +24,7 @@ class ProductController extends Controller
     {
         $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
 
-        return ['products' => $products];
+        return $this->render('product/list.html.twig', ['products' => $products]);
     }
 
     /**
@@ -30,5 +33,31 @@ class ProductController extends Controller
     public function showAction(Product $product)
     {
         return new JsonResponse($product);
+    }
+
+    /**
+     * @Route("/new", name="products_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->add('save', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Product was saved.');
+
+            return $this->redirectToRoute("products_list");
+        }
+
+
+        return $this->render('product/new.html.twig', ['form' => $form->createView()]);
     }
 }
